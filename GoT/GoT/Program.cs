26b0357c393052e3beed6dddd60fe601a60bd1c5
boahs <39522778+boahs@users.noreply.github.com>
2019-05-23 00:07:46 -0400

    Add files via upload

---
+++ GoT/GoT/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Game of thrones season 1 ");
+            Console.WriteLine("Good");
+            Console.WriteLine("Game of thrones season 2 ");
+            Console.WriteLine("Good");
+            Console.WriteLine("Game of thrones season 3 ");
+            Console.WriteLine("Good");
+            Console.WriteLine("Game of thrones season 4 ");
+            Console.WriteLine("Good");
+            Console.WriteLine("Game of thrones season 5 ");
+            Console.WriteLine("Good");
+            Console.WriteLine("Game of thrones season 6 ");
+            Console.WriteLine("Good");
+            Console.WriteLine("Game of thrones season 7 ");
+            Console.WriteLine("Shit");
+            Console.WriteLine("Game of thrones season 8 ");
+            Console.WriteLine("Shit");
+
+        }
+    }
+}
