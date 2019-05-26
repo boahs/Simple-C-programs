305f617387b1d16b191e8f8a1e669fe9bbffb4e5
boahs <39522778+boahs@users.noreply.github.com>
2019-05-26 18:31:05 -0400

    Add files via upload

---
+++ GreeterNoticeArray/GreeterNoticeArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeterNoticeArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number;
+
+            string firstName = "Jordan";
+            string lastName = "Parsons";
+
+            string fullName = firstName + " " + lastName;
+            string myFullName = string.Format("my name is {0} {1}", firstName, lastName);
+
+            string[] names = new string[3] { "Jack", "John", "Tom" };
+            string formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+            string text = @"Hi Jordan 
+            Look into the following paths
+            c:\folder1\folder2
+            c:\folder3\folder4";
+            Console.WriteLine(text);
+
+
+
+
+        }
+
+    }
+}
